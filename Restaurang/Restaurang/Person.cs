f274fce52a7a72fef76bdb2ff6ed83038c95011a
majaelb <115992540+majaelb@users.noreply.github.com>
2022-10-25 18:10:52 +0200

    Update Person.cs

--- Restaurang/Restaurang/Person.cs
+++ Restaurang/Restaurang/Person.cs
@@ -8,6 +8,49 @@ namespace Restaurang
 {
     internal class Person
     {
-        public string N_A_M_N { get; set; }
+        public string Name { get; set; }
+        public int AmountOfPeople { get; set; }
+
+        
+
+        public Person()
+        {
+
+        }
+    }
+
+    internal class Guest : Person
+    {
+        public int AmountOfCompany { get; set; }
+        public double AmountOfMoney { get; set; }
+        public int Satisfaction { get; set; }
+        public bool Allergic { get; set; }
+
+        public Guest()
+        {
+
+        }
+
+    }
+
+    internal class Chef : Person
+    {
+        public int Skills { get; set; }
+
+        public Chef()
+        {
+
+        }
+
+    }
+
+    internal class Waiter : Person
+    {
+        public int ServiceLevel { get; set; }
+        public Waiter()
+        {
+
+        }
+
     }
 }
