e4b2d42d9791a2f7e18e6761bad4d59514b91a20
lunibu <110604915+lunibu@users.noreply.github.com>
2022-10-29 10:57:28 +0200

    Skapat klasser Menu, Dish och subklasser MeatDish, FishDish, VegoDish utan Allergener.

---
+++ Restaurang/Restaurang/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurang
+{
+    internal class Menu
+    {
+        List<Dish> menus = new List<Dish>();
+    }
+    internal class Dish
+    {
+        internal string Name { get; set; }
+        internal int Price { get; set; }
+        internal Dish(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+        internal class MeatDish : Dish
+        {
+            internal MeatDish(string name, int price) : base(name, price)
+            {
+            }
+        }
+        internal class FishDish : Dish
+        {
+            internal FishDish(string name, int price) : base(name, price)
+            {
+            }
+        }
+        internal class VegoDish : Dish
+        {
+            internal VegoDish(string name, int price) : base(name, price)
+            {
+            }
+        }
+    }
+}
+
