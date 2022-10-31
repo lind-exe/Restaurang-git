82e5e0c5b36284b112c235254ca586583bf82be7
lind-exe <jojje_lind@live.se>
2022-10-31 13:54:58 +0100

    GUI commit

---
+++ GUI/GUI/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] todoList = new string[3];
+            todoList[0] = "Diska";
+            todoList[1] = "Mata katten";
+            todoList[2] = "Städa";
+
+            Window.Draw("Att göra", 5, 5, todoList);
+
+        }
+    }
+}
---
+++ GUI/GUI/Window.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class Window
+    {
+        public static void Draw(string header, int fromLeft, int fromTop, string[] graphics)
+        {
+
+            int width = 0;
+            for (int i = 0; i < graphics.Length; i++)
+            {
+                if (graphics[i].Length > width)
+                {
+                    width = graphics[i].Length;
+                }
+            }
+            if (width < header.Length + 4)
+            { width = header.Length + 4; };
+
+            Console.SetCursorPosition(fromLeft, fromTop);
+            if (header != "")
+            {
+                Console.Write('┌' + " ");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(header);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" " + new String('─', width - header.Length) + '┐');
+            }
+            else
+            {
+                Console.Write('┌' + new String('─', width + 2) + '┐');
+            }
+            Console.WriteLine();
+            int maxRows = 0;
+            for (int j = 0; j < graphics.Length; j++)
+            {
+                Console.SetCursorPosition(fromLeft, fromTop + j + 1);
+                Console.WriteLine('│' + " " + graphics[j] + new String(' ', width - graphics[j].Length + 1) + '│');
+                maxRows = j;
+            }
+            Console.SetCursorPosition(fromLeft, fromTop + maxRows + 2);
+            Console.Write('└' + new String('─', width + 2) + '┘');
+
+        }
+    }
+}
